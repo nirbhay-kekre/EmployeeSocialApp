5da142b3a050ac659bcbe36de55f3ee40d9009e5
RajatC13 <theimagineer.rajat@gmail.com>
2019-04-10 16:15:08 -0700

    get and delete employer apis done

--- src/main/java/edu/sjsu/cmpe275/api/controller/implementation/EmployerAPIController.java
+++ src/main/java/edu/sjsu/cmpe275/api/controller/implementation/EmployerAPIController.java
@@ -1,25 +1,42 @@
 package edu.sjsu.cmpe275.api.controller.implementation;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import edu.sjsu.cmpe275.api.controller.interfaces.IEmployerAPI;
 import edu.sjsu.cmpe275.api.model.Employer;
+import edu.sjsu.cmpe275.api.repository.EmployeeRepository;
+import edu.sjsu.cmpe275.api.repository.EmployerRepository;
 
 @Controller
 public class EmployerAPIController implements IEmployerAPI {
+	
+	@Autowired
+	private EmployerRepository employerRepository;
+
+	@Autowired
+	ObjectMapper mapper;
 
 	@Override
 	public ResponseEntity<Employer> getEmployer(Long id, String format) {
 		String type = "application/" + format.toLowerCase();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Type", type + "; charset=UTF-8");
-
-		// TODO: API logic
-
-		return new ResponseEntity<Employer>(new Employer(), headers, HttpStatus.NOT_IMPLEMENTED);
+		
+		Optional<Employer> emplr = employerRepository.findById(id);
+		if(emplr.isPresent()) {
+			Employer employer = emplr.get();
+			return new ResponseEntity<Employer>(employer, headers, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<Employer>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 	@Override
@@ -27,10 +44,21 @@ public class EmployerAPIController implements IEmployerAPI {
 		String type = "application/" + format.toLowerCase();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Type", type + "; charset=UTF-8");
+		
+		Optional<Employer> emplr = employerRepository.findById(id);
+		if(emplr.isPresent()) {
+			employerRepository.deleteById(id);
+			Optional<Employer> emplr_check = employerRepository.findById(id);
+			if(emplr_check.isPresent()) {
+				return new ResponseEntity<Employer>(HttpStatus.BAD_REQUEST);
+			}else {
+				return new ResponseEntity<Employer>(HttpStatus.OK);
+			}
+			
+		}else {
+			return new ResponseEntity<Employer>(HttpStatus.NOT_FOUND);
+		}
 
-		// TODO: API logic
-
-		return new ResponseEntity<Employer>(new Employer(), headers, HttpStatus.NOT_IMPLEMENTED);
 	}
 
 	@Override
