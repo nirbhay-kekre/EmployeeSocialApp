898209a7108b552f57dbcda626e4711946e640ce
RajatC13 <theimagineer.rajat@gmail.com>
2019-04-14 12:46:46 -0700

    minor fixes

--- src/main/java/edu/sjsu/cmpe275/api/model/Employer.java
+++ src/main/java/edu/sjsu/cmpe275/api/model/Employer.java
@@ -9,16 +9,16 @@ import javax.persistence.Id;
 
 @Entity
 public class Employer {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	
-	@Column(unique=true, length=100)
+
+	@Column(unique = true, length = 100)
 	private String name;
-	
+
 	private String description;
-	
+
 	@Embedded
 	private Address address;
 
@@ -58,10 +58,7 @@ public class Employer {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((address == null) ? 0 : address.hashCode());
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -71,25 +68,10 @@ public class Employer {
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof Employer))
 			return false;
 		Employer other = (Employer) obj;
-		if (address == null) {
-			if (other.address != null)
-				return false;
-		} else if (!address.equals(other.address))
-			return false;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
+		if (id != other.getId())
 			return false;
 		return true;
 	}
